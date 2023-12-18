7f0b5f6e8ed1ee28544c1f5c647705a6df801ae1
nephriteagain <oneabove232@gmail.com>
2023-12-18 15:26:02 +0800

    style: scroll to top when show quiz is clicked

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,32 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("connect to db"))
-    .catch((err) => console.log(err));
+
+
+
+async function connectToMongo(tries) {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to DB');
+    } catch (error) {
+        console.error({ error, tries });
+        if (tries.retries < 5) {
+            // Retry with an increased retry count
+            ++tries.retries;
+            await connectToMongo(tries);
+        } else {
+            console.error('Max retries reached. Unable to connect to MongoDB.');
+        }
+    }
+}
+
+(async function(){
+
+    const tries = {retries:0} 
+    try {
+        await connectToMongo(tries)        
+    } catch (error) {
+        console.error({error, message: `failed to connect to MONGODB, retried ${tries.retries} times`})
+    }
+
+})()
